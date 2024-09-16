e0ab30f94ecc997fe2620fb69af1776d049d1d3d
JDthecreator <jetlyfejae@gmail.com>
2024-09-16 12:25:17 -0400

    Added id for buttons, toggle func, font styling.

--- main.js
+++ main.js
@@ -4,9 +4,17 @@
 const buttons = document.querySelectorAll(".panelbutton");
 const panels = document.querySelectorAll(".panel1, .panel2, .panel3, .panel4");
 
+
 // Loop through all buttons and attach a click event
 buttons.forEach((button, index) => {
   button.addEventListener("click", () => {
+    const image = button.querySelector("img");
+     
+    if (image.getAttribute("src") == "./assets/images/icon-plus.svg") {
+      image.setAttribute("src","./assets/images/icon-minus.svg" )  
+    } else {
+      image.setAttribute("src", "./assets/images/icon-plus.svg")};   
+ 
     // Toggle the display of the corresponding panel
     const panel = panels[index];
     if (panel.style.display === "none" || panel.style.display === "") {
